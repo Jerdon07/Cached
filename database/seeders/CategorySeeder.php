346696a51418dbe4b0c96bab842fee812ff6a469
Jerdon07 <jdlitaoen@gmail.com>
2026-08-04 22:47:27 +0800

    FEAT: Create category model and its necessary classes

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Hardware'],
+            ['name' => 'Electrical'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::firstOrCreate($category);
+        }
+    }
+}

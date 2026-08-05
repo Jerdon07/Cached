89e7259f20296422653fff2a00d3ad36208e410d
Jerdon07 <jdlitaoen@gmail.com>
2026-08-05 12:22:39 +0800

    FIX: Fix typo propagated from migration to model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['name', 'desctiption'])]
+#[Fillable(['name', 'description'])]
 class Brand extends Model
 {
     //

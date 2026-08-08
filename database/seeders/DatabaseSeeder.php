fc509abe19958dd238b8fd5fef9d46fd31deb9f1
Jerdon07 <jdlitaoen@gmail.com>
2026-08-08 13:03:40 +0800

    FEAT: Add PurchaseOrderItemSeeder to seed purchase order items

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             ProductSupplierSeeder::class,
 
             PurchaseOrderSeeder::class,
+            PurchaseOrderItemSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/PurchaseOrderItemSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PurchaseOrderItem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PurchaseOrderItemSeeder extends Seeder
+{
+    public function run(): void
+    {
+        PurchaseOrderItem::firstOrCreate([
+            'purchase_order_id' => 1,
+            'product_id' => 1
+        ], [
+            'quantity' => 20.00,
+            'unit_cost' => 50.00,
+            'subtotal' => 50.00,
+        ]);
+    }
+}

81476111b442df4cbdbee9b003a9a111f41c70a7
Jerdon07 <jdlitaoen@gmail.com>
2026-08-12 11:55:14 +0800

    FIX: Change the remarks field to notes

--- database/seeders/PurchaseOrderSeeder.php
+++ database/seeders/PurchaseOrderSeeder.php
@@ -11,18 +11,15 @@ class PurchaseOrderSeeder extends Seeder
 {
     public function run(): void
     {
-        PurchaseOrder::firstOrCreate(
-            ['po_number' => 'PO-2026-0001'],
-            [
-                'supplier_id' => 1,
-                'order_date' => now(),
-                'expected_delivery_date' => now()->addDays(3),
-                'status' => PurchaseOrderStatus::Pending,
-                'remarks' => 'A nice purchase order from a supplier.',
-                'created_by' => 'Zuck Muckerberg',
-                'approved_by' => 'Elong Mah',
-                'approved_at' => now()->addHour(),
-            ]
-        );
+        PurchaseOrder::create([
+            'supplier_id' => 1,
+            'order_date' => now(),
+            'expected_delivery_date' => now()->addDays(3),
+            'status' => PurchaseOrderStatus::Pending,
+            'notes' => 'A nice purchase order from a supplier.',
+            'created_by' => 'Zuck Muckerberg',
+            'approved_by' => 'Elong Mah',
+            'approved_at' => now()->addHour(),
+        ]);
     }
 }

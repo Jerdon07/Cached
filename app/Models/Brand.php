01ae137b3b3e76e9f5a7b30c6a92bbf07c2d1487
Jerdon07 <jdlitaoen@gmail.com>
2026-08-12 15:00:47 +0800

    REFACTOR: Add soft delete to the brands table

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'description'])]
 class Brand extends Model
 {
+    use SoftDeletes;
+
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);

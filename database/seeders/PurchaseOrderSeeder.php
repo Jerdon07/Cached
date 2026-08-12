f65c518c22bb9a10b356a81861340e212a401728
Jerdon07 <jdlitaoen@gmail.com>
2026-08-12 14:41:22 +0800

    FIX: Change the created_by and approved_by into integer

--- database/seeders/PurchaseOrderSeeder.php
+++ database/seeders/PurchaseOrderSeeder.php
@@ -17,8 +17,7 @@ class PurchaseOrderSeeder extends Seeder
             'expected_delivery_date' => now()->addDays(3),
             'status' => PurchaseOrderStatus::Pending,
             'notes' => 'A nice purchase order from a supplier.',
-            'created_by' => 'Zuck Muckerberg',
-            'approved_by' => 'Elong Mah',
+            'created_by' => 1,
             'approved_at' => now()->addHour(),
         ]);
     }

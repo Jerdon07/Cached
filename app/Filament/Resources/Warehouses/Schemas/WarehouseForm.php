f666ceb221ed014bc00fc1cd794ef88933464d59
Jerdon07 <jdlitaoen@gmail.com>
2026-08-13 19:15:20 +0800

    FEAT: Create filament resource for warehouse

---
+++ app/Filament/Resources/Warehouses/Schemas/WarehouseForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Warehouses\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class WarehouseForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('address')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+            ]);
+    }
+}

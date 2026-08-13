6eb78b40bec10cb707d29f1e4396d024315b11c0
Jerdon07 <jdlitaoen@gmail.com>
2026-08-13 19:57:08 +0800

    FEAT: Add TextEntries

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use App\Models\Product;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -10,7 +13,25 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                //
+                TextEntry::make('name'),
+                TextEntry::make('sku')
+                    ->placeholder('-'),
+                TextEntry::make('barcode'),
+                TextEntry::make('category.name'),
+                TextEntry::make('brand.name'),
+                TextEntry::make('unit.abbrevation'),
+                TextEntry::make('selling_price')
+                    ->money('PHP'),
+                TextEntry::make('minimum_stock')
+                    ->numeric(),
+                IconEntry::make('is_active'),
+                TextEntry::make('created_at')
+                    ->dateTime(),
+                TextEntry::make('updated_at')
+                    ->dateTime(),
+                TextEntry::make('deleted_at')
+                    ->dateTime()
+                    ->visible(fn (Product $record): bool => $record->trashed()),
             ]);
     }
 }

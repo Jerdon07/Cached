52c4a375c9ba70fe6d89d8ee06c9f7c60c434ed0
Jerdon07 <jdlitaoen@gmail.com>
2026-08-14 21:13:55 +0800

    FEAT: Implement product creation notification and redirect functionality

--- app/Filament/Resources/Products/Pages/CreateProduct.php
+++ app/Filament/Resources/Products/Pages/CreateProduct.php
@@ -3,9 +3,29 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Override;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    #[Override]
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    #[Override]
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Product Created')
+            ->body('Product is created successfully');
+    }
+
 }

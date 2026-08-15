6e1e305803d2ac2ca44bc6f1923fabce70cd8523
Jerdon07 <jdlitaoen@gmail.com>
2026-08-15 19:39:26 +0800

    FEAT: Create policy for supplier

---
+++ app/Policies/SupplierPolicy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Supplier;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SupplierPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermissionTo('view_any_suppliers');
+    }
+
+    public function view(User $user, Supplier $supplier): bool
+    {
+        return $user->hasPermissionTo('view_suppliers');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->hasPermissionTo('create_suppliers');
+    }
+
+    public function update(User $user, Supplier $supplier): bool
+    {
+        return $user->hasPermissionTo('update_suppliers');
+    }
+
+    public function deleteAny(User $user): bool
+    {
+        return $user->hasPermissionTo('delete_any_suppliers');
+    }
+
+    public function delete(User $user, Supplier $supplier): bool
+    {
+        return $user->hasPermissionTo('delete_suppliers');
+    }
+}

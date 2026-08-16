41df5e678208b6e00e14cf6853473279ece199e8
Jerdon07 <jdlitaoen@gmail.com>
2026-08-16 20:48:28 +0800

    REFACTOR: Remove view action

--- app/Filament/Resources/Suppliers/Pages/EditSupplier.php
+++ app/Filament/Resources/Suppliers/Pages/EditSupplier.php
@@ -14,7 +14,6 @@ class EditSupplier extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            ViewAction::make(),
             DeleteAction::make(),
         ];
     }

756793ede41323ab93a0f0f874aabe43403c7d42
Jerdon07 <jdlitaoen@gmail.com>
2026-08-16 21:14:29 +0800

    FEAT: Create policy for customer model

---
+++ app/Policies/CustomerPolicy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CustomerPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermissionTo('view_any_customers');
+    }
+
+    public function view(User $user, Customer $customer): bool
+    {
+        return $user->hasPermissionTo('view_customers');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->hasPermissionTo('create_customers');
+    }
+
+    public function update(User $user, Customer $customer): bool
+    {
+        return $user->hasPermissionTo('update_customers');
+    }
+
+    public function deleteAny(User $user): bool
+    {
+        return $user->hasPermissionTo('delete_any_customers');
+    }
+
+    public function delete(User $user, Customer $customer): bool
+    {
+        return $user->hasPermissionTo('delete_customers');
+    }
+}

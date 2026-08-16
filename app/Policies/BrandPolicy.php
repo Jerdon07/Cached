11ffd2632fc5fb32208f52de443b188d475929bf
Jerdon07 <jdlitaoen@gmail.com>
2026-08-16 20:21:41 +0800

    FIX: Fix permission typo

--- app/Policies/BrandPolicy.php
+++ app/Policies/BrandPolicy.php
@@ -10,51 +10,51 @@ class BrandPolicy
 {
     public function viewAny(User $user): bool
     {
-        return $user->hasPermissionTo('view_any_categories');
+        return $user->hasPermissionTo('view_any_brands');
     }
 
     public function view(User $user, Brand $brand): bool
     {
-        return $user->hasPermissionTo('view_categories');
+        return $user->hasPermissionTo('view_brands');
     }
 
     public function create(User $user): bool
     {
-        return $user->hasPermissionTo('create_categories');
+        return $user->hasPermissionTo('create_brands');
     }
 
     public function update(User $user, Brand $brand): bool
     {
-        return $user->hasPermissionTo('update_categories');
+        return $user->hasPermissionTo('update_brands');
     }
 
     public function deleteAny(User $user): bool
     {
-        return $user->hasPermissionTo('delete_any_categories');
+        return $user->hasPermissionTo('delete_any_brands');
     }
 
     public function delete(User $user, Brand $brand): bool
     {
-        return $user->hasPermissionTo('delete_categories');
+        return $user->hasPermissionTo('delete_brands');
     }
 
     public function restoreAny(User $user): bool
     {
-        return $user->hasPermissionTo('restore_any_categories');
+        return $user->hasPermissionTo('restore_any_brands');
     }
 
     public function restore(User $user, Brand $brand): bool
     {
-        return $user->hasPermissionTo('restore_categories');
+        return $user->hasPermissionTo('restore_brands');
     }
 
     public function forceDeleteAny(User $user): bool
     {
-        return $user->hasPermissionTo('force_delete_any_categories');
+        return $user->hasPermissionTo('force_delete_any_brands');
     }
 
     public function forceDelete(User $user, Brand $brand): bool
     {
-        return $user->hasPermissionTo('force_delete_categories');
+        return $user->hasPermissionTo('force_delete_brands');
     }
 }

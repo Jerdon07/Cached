e43b90a5197a0241d7246fd6549d216aac22578c
Jerdon07 <jdlitaoen@gmail.com>
2026-08-16 21:42:50 +0800

    FEAT: Create sales order relation manager for customer resource

---
+++ app/Filament/Resources/Customers/RelationManagers/SalesOrderRelationManager.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources\Customers\RelationManagers;
+
+use App\SalesOrderStatus;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SalesOrderRelationManager extends RelationManager
+{
+    protected static string $relationship = 'SalesOrders';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                DateTimePicker::make('order_date')
+                    ->displayFormat('d/M/Y H:i')
+                    ->required(),
+                Select::make('status')
+                    ->options(SalesOrderStatus::class)
+                    ->required()
+                    ->preload(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('order_date')
+            ->columns([
+                TextColumn::make('order_date')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('status')
+                    ->badge()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true)
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->recordActions([
+                ActionGroup::make([
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}

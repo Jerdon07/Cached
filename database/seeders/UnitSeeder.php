dbd975fa4c7d354fda38a7d41b02591270e462c9
Jerdon07 <jdlitaoen@gmail.com>
2026-08-17 19:39:25 +0800

    REFACTOR: Add other unit of measurements for tests

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -11,8 +11,28 @@ class UnitSeeder extends Seeder
     public function run(): void
     {
         $units = [
-            ['name' => 'Piece', 'abbreviation' => 'pc'],
-            ['name' => 'Box', 'abbreviation' => 'box'],
+            ['name' => 'Piece', 'abbreviation' => 'PC'],
+            ['name' => 'Box', 'abbreviation' => 'BX'],
+            ['name' => 'Each', 'abbreviation' => 'EA'],
+            ['name' => 'Pair', 'abbreviation' => 'PR'],
+            ['name' => 'Set', 'abbreviation' => 'SET'],
+            ['name' => 'Package', 'abbreviation' => 'PK'],
+            ['name' => 'Carton', 'abbreviation' => 'CTN'],
+            ['name' => 'Case', 'abbreviation' => 'CS'],
+            ['name' => 'Dozen', 'abbreviation' => 'DZ'],
+            ['name' => 'Bundle', 'abbreviation' => 'BDL'],
+            ['name' => 'Roll', 'abbreviation' => 'RL'],
+            ['name' => 'Bag', 'abbreviation' => 'BG'],
+            ['name' => 'Pallet', 'abbreviation' => 'PLT'],
+            ['name' => 'Skid', 'abbreviation' => 'SKD'],
+            ['name' => 'Crate', 'abbreviation' => 'CRT'],
+            ['name' => 'Container', 'abbreviation' => 'CTR'],
+            ['name' => 'Kilogram', 'abbreviation' => 'KG'],
+            ['name' => 'Pound', 'abbreviation' => 'LB'],
+            ['name' => 'Litter', 'abbreviation' => 'L'],
+            ['name' => 'Gallon', 'abbreviation' => 'GAL'],
+            ['name' => 'Meter', 'abbreviation' => 'M'],
+            ['name' => 'Foot', 'abbreviation' => 'FT'],
         ];
 
         foreach ($units as $unit) {

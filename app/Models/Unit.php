7292a432520a4576704b48ce36f161db03a2fcf5
Jerdon07 <jdlitaoen@gmail.com>
2026-08-21 13:07:49 +0800

    REFACTOR: Add the fillable columns

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[Fillable(['name', 'abbreviation'])]
 class Unit extends Model
 {
     use SoftDeletes;

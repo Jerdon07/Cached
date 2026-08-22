c47793f8d9ff71d92aae13541a8e739d55e5a3f7
Jerdon07 <jdlitaoen@gmail.com>
2026-08-23 00:17:42 +0800

    FEAT: Add delete action to record actions in PurchaseOrdersTable

--- app/Filament/Resources/PurchaseOrders/Tables/PurchaseOrdersTable.php
+++ app/Filament/Resources/PurchaseOrders/Tables/PurchaseOrdersTable.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\PurchaseOrders\Tables;
 
+use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
@@ -45,10 +47,13 @@ class PurchaseOrdersTable
             ->filters([
                 //
             ])
-            ->recordActions([
-                ViewAction::make(),
-                EditAction::make(),
-            ])
+            ->recordActions(
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
+            )
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),

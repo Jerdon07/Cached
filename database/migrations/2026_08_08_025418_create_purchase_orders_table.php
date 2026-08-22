894e11b13116515332ee2a3c0e79eb814eb4051b
Jerdon07 <jdlitaoen@gmail.com>
2026-08-23 00:02:23 +0800

    FIX: Set the order date nd expected eliveery date as nullable

--- database/migrations/2026_08_08_025418_create_purchase_orders_table.php
+++ database/migrations/2026_08_08_025418_create_purchase_orders_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
             $table->foreignId('supplier_id')->constrained()->cascadeOnDelete();
             $table->foreignId('created_by')->nullable()->constrained('users')->restrictOnDelete();
             $table->foreignId('approved_by')->nullable()->constrained('users')->restrictOndelete();
-            $table->date('order_date');
-            $table->date('expected_delivery_date');
+            $table->date('order_date')->nullable();
+            $table->date('expected_delivery_date')->nullable();
             $table->enum('status', ['draft', 'pending', 'approved', 'rejected', 'cancelled', 'completed'])->default('draft');
             $table->text('notes')->nullable();
             $table->date('approved_at')->nullable();

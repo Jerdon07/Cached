c594043b9bffeec7be11c2ba0d3dbb3355bec5d5
Jerdon07 <jdlitaoen@gmail.com>
2026-08-23 00:05:35 +0800

    FEAT: Set the created_by as the creator and the status as pending

--- app/Filament/Resources/PurchaseOrders/Pages/CreatePurchaseOrder.php
+++ app/Filament/Resources/PurchaseOrders/Pages/CreatePurchaseOrder.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\PurchaseOrders\Pages;
 
 use App\Filament\Resources\PurchaseOrders\PurchaseOrderResource;
+use App\PurchaseOrderStatus;
 use Filament\Resources\Pages\CreateRecord;
+use Override;
 
 class CreatePurchaseOrder extends CreateRecord
 {
     protected static string $resource = PurchaseOrderResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = auth()->id();
+        $data['status'] = PurchaseOrderStatus::Pending;
+
+        return $data;
+    }
 }
